a48e733b177791ecf37a3a4b8aee1ea1db8fecc0
irenams <irenamst@gmail.com>
2016-04-19 11:09:38 +0300

    Update19.04.2016

--- Zad5(poslednite).cpp
+++ Zad5(poslednite).cpp
@@ -3,15 +3,17 @@ int check_Parity(int x);
 int bitcount(int x);
 int main() {
 	int x;
+	int p;
 	scanf_s("%x", &x);
-	printf("%d", check_Parity(x));
+	p = check_Parity(x);
+	printf("%d",p);
 	getchar();
 	return 0;
 }
 int check_Parity(int x) {
 	unsigned int b;
 	b = bitcount(x);
-	if (b & 1)
+	if (b % 2 == 0)
 		return 1;
 	else
 		return 0;
